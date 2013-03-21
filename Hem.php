893a3b046265eec82bdd70234be25026475293f5
Stefan <Stefan@Stefan-Dator>
2013-03-21 15:04:08 +0100

    Hem 1a sida.

---
+++ Hem.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http//www.w3.org/T/html4/loose.dtd">
+<html>
+    <head>
+        <link rel="shortcut icon" href="favicon.ico">
+        <META http-equiv="Content-Type" content="text/html;
+              charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="Cssmall.css">
+        <script language="javascript" type="text/javascript" src="javascript.js"></script>
+    </head>
+    <body>
+        <noscript>Din browser stödjer ej javascript!</noscript>
+        <div id="bakgrund">
+            <div id="mainwindow">
+                <?php
+                include("header.php");
+                include("Menyknappar.php");
+                ?>
+                <div id="SecondWindow">
+                    <div id="Hem">
+                       <img src="Bilder/Logo.Png">
+                                    <br>
+                                    <h1>TEXT TEXT TEXT</h1>
+                                    <br>
+                                   <img src="Bilder/Kök.jpg">  <img src="Bilder/Vardagsrum.jpg">
+                                    <img src="Bilder/Sovrum.jpg">          
+                                         
+                                </td
+
+                                </div>
+                                </div>
+
+                                </div>
+
+                                </div>
+                                </body>
+                                </html>
